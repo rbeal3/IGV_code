c532e96bdf812300fc00ef05a26c7b121352019d
Ryan Beal <rbeal3@mail.csuchico.edu>
2015-04-17 23:20:58 -0700

    updates oneStep, which still builds commands wrong

--- utils/oneStep.py
+++ utils/oneStep.py
@@ -9,20 +9,16 @@ import sys
 SLAVE = '192.168.1.2'
 PORT = 23
 
+# python oneStep.py 1 #will go forward 2 feet
+# python oneStep.py -r 2 # will turn right 180 degrees
+# 
+#
+#
 
 class App(object):
     def __init__(self):
 			self.reset()
 			self.font = pygame.font.Font(None, 20)
-    def task(self, socket):
-			cmd = "PR 0,0,0,0,10000,10000;BG;"
-			print "comand from task" + cmd
-			socket.sendall(cmd + '\r')
-			data = socket.recv(1024)
-			print data
-
-			print "if this ever runs, note it"
-			root.after(20, self.task, socket)
 
 def main(argv):
 
@@ -39,13 +35,17 @@ def main(argv):
 		sock.close()
 		sys.exit(1) 
 	print "Successfully connected, initializing acceleration variables"
-	cmd = 'AC 0,0,0,0,50000,50000; DC 0,0,0,0,50000,50000;\r'
+	cmd = 'AC 0,0,0,0,50000,50000;\r'
+	print cmd
+	sock.sendall(cmd)
+	cmd = 'DC 0,0,0,0,50000,50000;\r'
+	print cmd
 	sock.sendall(cmd)
 	cmd = 'OE 0,0,0,0,1,1,0,0;\r'#error fail ON
+	print cmd
 	sock.sendall(cmd)
 	cmd = 'ER ,,,,32767,32767,,;\r'#error fail setting
-	sock.sendall(cmd)
-	cmd = 'KI 0,0,0,0,0.1,0.1,0,0;\r'#motor gain adjustment
+	print cmd
 	sock.sendall(cmd)
 #handle args
 	try:
@@ -53,28 +53,40 @@ def main(argv):
 		opts, args = getopt.getopt(argv, 'lr')
 	except getopt.GetoptError:
 		print 'failed to parse cmd line'
-	if args[0] == '': #go straight
+	if len(sys.argv)==1:
 		print "you need to include a turn distance -i for inverse direction"
-	elif args[0] == '0': #go straight
-		print "Gir Will now take one step!\n"
-		cmd = 'PR 0,0,0,0,10000,10000; BG;\r'
+		return 0;
+
+	turnAmount=args[0]*25000
+	distance=args[0]*26500
+	cmd = 'PR 0,0,0,0,'+distance+','+distance+';\r'
+
+	print "distance =" + distance
+	print "the command will be:"
+	print cmd
+	time.sleep(5)
+
 	for o, a in opts:
-		if o in ("-l"): #left turn
-			print "Gir Will now turn left!\n"
-			cmd = 'PR 0,0,0,0,-'+args[0]+','+args[0]+'; BG;\r'
-		elif o in ("-r"):
+		if o in ("-r"): #right turn
 			print "Gir Will now turn right!\n"
-			cmd = 'PR 0,0,0,0,'+args[0]+',-'+args[0]+'; BG;\r'
+			cmd = 'PR 0,0,0,0,-'+turnAmount+','+turnAmount+';\r'
+		elif o in ("-l"):
+			print "Gir Will now turn left!\n"
+			cmd = 'PR 0,0,0,0,'+turnAmount+',-'+turnAmount+';\r'
 		
 		#do something with 
 	sock.sendall(cmd)
 	data = sock.recv(1024)
+	print "turn command sent:"
 	print cmd
 	print data
 	cmd = 'BG;\r'
 	sock.sendall(cmd)
 	data = sock.recv(1024)
 	time.sleep(0.1)
+
+	print "command sent: (hopefully BG)"
+	print cmd
 
 #code to bot above this
 	try:
